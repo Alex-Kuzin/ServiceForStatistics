77a393a14125684cf413db0f7faa66608b4cde06
Alex Kuzin <acidkarma3000@gmail.com>
2023-06-01 16:32:42 +0500

    Rework7

--- src/test/java/ru/netology/StatsServiceTest.java
+++ src/test/java/ru/netology/StatsServiceTest.java
@@ -52,8 +52,7 @@ public class StatsServiceTest {
     }
 
     @Test
-    public void average()
-    {
+    public void average() {
         StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedMonth = 5;
